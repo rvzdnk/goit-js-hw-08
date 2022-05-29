31bf7779b6ea8c8ef07368f5fd23901315932141
rvzdnk <rafal93zdunek@gmail.com>
2022-05-29 20:55:37 +0200

    commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,50 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector(".gallery")
+
+const markup = galleryItems
+.map(({preview, original, description}) => 
+`<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`)
+  .join("");
+
+
+  gallery.insertAdjacentHTML("afterbegin", markup);
+  
+  gallery.addEventListener("click", lightboxModal)
+
+  gallery.onclick = function lightboxModal (e){
+    e.preventDefault();
+    
+    if (e.target.nodeName !== "IMG"){
+        return;
+    };
+    
+    const instance = basicLightbox.create(
+        `<img width="1400" height="900" src = ${e.target.dataset.source}>`
+    );
+    instance.show();
+
+    document.addEventListener("keydown", (e) =>{
+        if (e.key === "Escape") {
+          instance.close();
+          }
+        });
+    }
+
+
+
+
