98f223e979ad46d8a8df6bdd6c4b0998280ecf44
rvzdnk <rafal93zdunek@gmail.com>
2022-05-30 21:33:56 +0200

    commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
 
+    import Player from '@vimeo/player';
+
+    const throttle = require('lodash.throttle');
+    
+    const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+    player.on('timeupdate', throttle(saveWatchTime, 1000));
+
+    function saveWatchTime(e){
+        localStorage.setItem(`videoplayer-current-time`, e.seconds);
+    }
+
+    player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
+   
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,26 @@
+const throttle = require('lodash.throttle');
+
+const form = document.querySelectorAll(".feedback-form")
+
+form.addEventListener("input",throttle(formInput,500));
+
+function formInput (e){
+    localStorage.setItem("feedback-form-state", JSON.stringify(createField(e)));
+}
+
+function createField (){
+    return{
+        email: form.elements.email.value,
+        message: form.elements.message.value,   
+    }
+} 
+
+form.addEventListener("submit", formSubmit)
+
+function formSubmit(e){
+    e.preventDefault();
+    console.log(createField());
+    form.reset();
+    localStorage.removeItem("feedback-form-state");
+}
 
