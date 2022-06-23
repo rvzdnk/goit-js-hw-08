c611b835dc0e0a2ccef73d2360c358763c161453
rvzdnk <rafal93zdunek@gmail.com>
2022-06-23 18:19:35 +0200

    bugfix task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,25 +2,42 @@ const throttle = require('lodash.throttle');
 
 const form = document.querySelectorAll(".feedback-form")
 
-form.addEventListener("input",throttle(formInput,500));
+form.addEventListener("input",throttle(formInput,500, {'trailing': false}));
 
 function formInput (e){
     localStorage.setItem("feedback-form-state", JSON.stringify(createField(e)));
 }
 
 function createField (){
-    return{
-        email: form.elements.email.value,
-        message: form.elements.message.value,   
+    if (JSON.parse(localStorage.getItem("feedback-form-state")) === null) {
+        return;
+      }
+    
+      const {email, message} = form.elements;
+    
+      const getValues = JSON.parse(localStorage.getItem("feedback-form-state"));
+    
+      email.value = getValues.email;
+      message.value = getValues.message;  
     }
-} 
+
 
 form.addEventListener("submit", formSubmit)
 
 function formSubmit(e){
     e.preventDefault();
-    console.log(createField());
+    console.log(currentFormValues(e));
     form.reset();
     localStorage.removeItem("feedback-form-state");
 }
 
+function currentFormValues(e) {
+    const formValues = {};
+  
+    const {email, message} = e.currentTarget.elements;
+  
+    formValues.email = email.value;
+    formValues.message = message.value;
+  
+    return formValues;
+  }
