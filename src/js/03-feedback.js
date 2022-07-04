7dd1c5080ffdd690e0461f02b3240ecd52f77826
rvzdnk <rafal93zdunek@gmail.com>
2022-07-04 18:01:38 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,43 +1,14 @@
 // Import lodash.throttle library
-import throttle from 'lodash.throttle';
+const throttle = require('lodash.throttle');
 
 // QS
-const form = document.querySelector(".feedback-form");
+const form = document.querySelector('.feedback-form');
 
-// Event listener to inout data to local storage
-form.addEventListener("input", throttle(addToStorage, 500));
+// Event listener
+form.addEventListener('input', throttle(addToStorage, 500, { trailing: false }));
+form.addEventListener("submit", resetStorage);
 
-// Function which save the data to local storage
-function addToStorage (e){
-    localStorage.setItem("feedback-form-state", JSON.stringify(inputValues(e)));
-}
-
-
-//Function which reload values from local stroage
-function ReloadValuesFromStorage() {
-    if (JSON.parse(localStorage.getItem("feedback-form-state")) === null){
-    return;
-    }
-
-    const {email, message} = form.elements;
-
-    const getValues = JSON.parse(localStorage.getItem("feedback-form-state"));
-
-    getValues.email = email.value;
-    getValues.message = message.value;
-}
-
-ReloadValuesFromStorage();
-
-//Function which clean the storage
-form.addEventListener("submit", clearStorage);
-
-function clearStorage(e){
-    e.preventDefault();
-    console.log(inputValues(e));
-    localStorage.removeItem("feedback-form-state");
-    e.currentTarget.reset();
-}
+reloadValuesFromStorage()
 
 // Function which return the current values of the input form
 function inputValues (e){
@@ -53,3 +24,47 @@ function inputValues (e){
 
      return formValues;
 }
+
+
+
+//Function which reload values from local stroage
+function reloadValuesFromStorage() {
+    if (jsonParse('feedback-form-state') === null){
+    return;
+    }
+
+    let getValues = jsonParse('feedback-form-state');
+
+    form.elements.email.value = getValues.email;
+    form.elements.message.value = getValues.message;
+}
+
+
+//Function which clean the storage
+function resetStorage(e){
+    e.preventDefault();
+    console.log(inputValues(e));
+    localStorage.removeItem('feedback-form-state');
+    form.reset();
+}
+
+// Function which save the data to local storage
+function addToStorage (e){
+    jsonStringify('feedback-from-state', inputValues(e));
+}
+
+function jsonStringify(key, func) {
+    try {
+      localStorage.setItem(key, JSON.stringify(func));
+    } catch (error) {
+      console.log(error.name);
+    }
+  }
+
+  function jsonParse(key) {
+    try {
+      return JSON.parse(localStorage.getItem(key));
+    } catch (error) {
+      console.log(error.name);
+    }
+  }
